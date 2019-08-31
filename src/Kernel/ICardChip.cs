30694c6fbb834d12b368eb9ed141b4a25b8a8140
herocod3r <jethromain@gmail.com>
2019-08-31 21:59:10 +0100

    added i card chip

---
+++ src/Kernel/ICardChip.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Kernel
+{
+    public interface ICardChip : IDisposable
+    {
+        byte[] CardId { get; } //This should contain the ATR info
+        Task<byte[]> TransmitCommandAsync(byte[] command);
+    }
+}
